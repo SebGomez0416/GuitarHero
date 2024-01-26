8a4a630a427ff5d46e6d5027f7c0b7e65efc3105
sebastian <sebbastian.gc@gmail.com>
2024-01-26 12:49:19 -0300

    Update menu input "WIP"

--- Assets/Scripts/UI/UI.cs
+++ Assets/Scripts/UI/UI.cs
@@ -1,45 +1,57 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 
 public class UI : MonoBehaviour
 {
     [SerializeField] private GameObject menu;
     [SerializeField] private GameObject play;
+    [SerializeField] private GameObject options;
     [SerializeField] private GameObject credits;
     
     private void Awake()
     {
         Cursor.visible = false;
     }
-
-    private void Update()
+    
+    public void Back(InputAction.CallbackContext callbackContext)
     {
-        /*if (!Input.GetKeyDown(KeyCode.Escape)) return;
         play.SetActive(false);
+        options.SetActive(false);
         credits.SetActive(false);
-        menu.SetActive(true);*/
+        menu.SetActive(true);
     }
 
-    public void Play()
+    public void Play(InputAction.CallbackContext callbackContext)
     {
+        if (!callbackContext.performed) return;
         menu.SetActive(false);
         play.SetActive(true);
     }
 
-    public void Credits()
+    public void Options(InputAction.CallbackContext callbackContext)
     {
+        
+        menu.SetActive(false);
+        options.SetActive(true);
+    }
+    
+    public void Credits(InputAction.CallbackContext callbackContext)
+    {
+        if (!callbackContext.performed) return;
         menu.SetActive(false);
         credits.SetActive(true);
     }
 
-    public void Exit()
+    public void Exit(InputAction.CallbackContext callbackContext)
     {
+        if (!callbackContext.performed) return;
         Application.Quit();
     }
-
+    
     public void ChangeScene(string scene)
     {
         SceneManager.LoadScene(scene);
     }
-    
+
 }
